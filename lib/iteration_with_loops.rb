4e67b8e536b8992e9de525cfc5562dbdb9c384af
interactive-directory-1839 <benthedunn@gmail.com>
2020-05-26 17:37:54 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -8,9 +8,9 @@ def join_nested_strings(src)
   do
     element_index = 0 
     while element_index < src[row_index].count if
-    src[row_index][element_index].is_a? int
-      
+    src[row_index][element_index].is_a? Integer
       element_index += 1
+      
     end
     row_index += 1 
 end
